89dcf9c5351476038847e882281da0d9dd423f09
daemon-r7 <daemon@coremedia-solution.com>
2017-07-04 21:47:03 +0200

    update

--- d43M0n23-Kali-gnome.sh
+++ d43M0n23-Kali-gnome.sh
@@ -18,7 +18,11 @@ apt-get install curl git live-build cdebootstrap devscripts -y
 
 # Clone the default Kali live-build config.
 
-git clone git://git.kali.org/live-build-config.git
+cd /root/c0r3/02-workspace/
+if [ ! -d live-build-config ]; then
+	git clone git://git.kali.org/live-build-config.git
+fi
+
 
 # Get the source package of the debian installer. 
 # The default Kali preseed file lives here, and will need changing for non-root user support.
@@ -42,6 +46,11 @@ debian-installer-launcher
 alsa-tools
 locales-all
 xorg
+bettercap
+screenfetch
+figlet
+terminator
+xrdp
 #kali-linux-top10
 EOF
 
@@ -54,6 +63,122 @@ label install
     append vga=788 -- quiet file=/cdrom/install/preseed.cfg locale=de_AT keymap=de hostname=d43M0n23 domain=local.lan
 EOF
 
+
+# Add .bash_rc
+cat << EOF > kali-config/common/includes.chroot/root/.bashrc
+# ~/.bashrc: executed by bash(1) for non-login shells.
+# see /usr/share/doc/bash/examples/startup-files (in the package bash-doc)
+# for examples
+
+# If not running interactively, don't do anything
+case $- in
+    *i*) ;;
+      *) return;;
+esac
+
+# don't put duplicate lines or lines starting with space in the history.
+# See bash(1) for more options
+HISTCONTROL=ignoreboth
+
+# append to the history file, don't overwrite it
+shopt -s histappend
+
+# for setting history length see HISTSIZE and HISTFILESIZE in bash(1)
+HISTSIZE=1000
+HISTFILESIZE=2000
+
+# check the window size after each command and, if necessary,
+# update the values of LINES and COLUMNS.
+shopt -s checkwinsize
+
+# If set, the pattern "**" used in a pathname expansion context will
+# match all files and zero or more directories and subdirectories.
+#shopt -s globstar
+
+# make less more friendly for non-text input files, see lesspipe(1)
+#[ -x /usr/bin/lesspipe ] && eval "$(SHELL=/bin/sh lesspipe)"
+
+# set variable identifying the chroot you work in (used in the prompt below)
+if [ -z "${debian_chroot:-}" ] && [ -r /etc/debian_chroot ]; then
+    debian_chroot=$(cat /etc/debian_chroot)
+fi
+
+# set a fancy prompt (non-color, unless we know we "want" color)
+case "$TERM" in
+    xterm-color) color_prompt=yes;;
+esac
+
+# uncomment for a colored prompt, if the terminal has the capability; turned
+# off by default to not distract the user: the focus in a terminal window
+# should be on the output of commands, not on the prompt
+force_color_prompt=yes
+
+if [ -n "$force_color_prompt" ]; then
+    if [ -x /usr/bin/tput ] && tput setaf 1 >&/dev/null; then
+	# We have color support; assume it's compliant with Ecma-48
+	# (ISO/IEC-6429). (Lack of such support is extremely rare, and such
+	# a case would tend to support setf rather than setaf.)
+	color_prompt=yes
+    else
+	color_prompt=
+    fi
+fi
+
+if [ "$color_prompt" = yes ]; then
+    PS1='${debian_chroot:+($debian_chroot)}\[\033[01;31m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
+else
+    PS1='${debian_chroot:+($debian_chroot)}\u@\h:\w\$ '
+fi
+unset color_prompt force_color_prompt
+
+# If this is an xterm set the title to user@host:dir
+case "$TERM" in
+xterm*|rxvt*)
+    PS1="\[\e]0;${debian_chroot:+($debian_chroot)}\u@\h: \w\a\]$PS1"
+    ;;
+*)
+    ;;
+esac
+
+# enable color support of ls and also add handy aliases
+if [ -x /usr/bin/dircolors ]; then
+    test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
+    alias ls='ls -all --color=auto'
+    #alias dir='dir --color=auto'
+    #alias vdir='vdir --color=auto'
+
+    #alias grep='grep --color=auto'
+    #alias fgrep='fgrep --color=auto'
+    #alias egrep='egrep --color=auto'
+fi
+
+# some more ls aliases
+#alias ll='ls -l'
+#alias la='ls -A'
+#alias l='ls -CF'
+
+# Alias definitions.
+# You may want to put all your additions into a separate file like
+# ~/.bash_aliases, instead of adding them here directly.
+# See /usr/share/doc/bash-doc/examples in the bash-doc package.
+
+if [ -f ~/.bash_aliases ]; then
+    . ~/.bash_aliases
+fi
+
+# enable programmable completion features (you don't need to enable
+# this, if it's already enabled in /etc/bash.bashrc and /etc/profile
+# sources /etc/bash.bashrc).
+if ! shopt -oq posix; then
+  if [ -f /usr/share/bash-completion/bash_completion ]; then
+    . /usr/share/bash-completion/bash_completion
+  elif [ -f /etc/bash_completion ]; then
+    . /etc/bash_completion
+  fi
+fi
+figlet d43M0n23-C0r3
+curl icanhazip.com
+EOF
 
 # SSH service start by default
 echo 'systemctl enable ssh' >>  kali-config/common/hooks/01-start-ssh.chroot
@@ -69,22 +194,22 @@ mv daemon3.png kali-config/common/includes.chroot/usr/share/wallpapers/kali/cont
 
 # unedetnt install
 mkdir -p kali-config/common/debian-installer
-wget https://raw.githubusercontent.com/offensive-security/kali-linux-preseed/master/kali-linux-full-unattended.preseed -O kali-config/common/debian-installer/preseed.cfg
+wget https://github.com/d43M0n23/0wnkali/kali-linux-full-unattended.preseed -O kali-config/common/debian-installer/preseed.cfg
 
 
 #Let’s include a Nessus Debian package into the packages directory for inclusion into our final build. 
 #Since we used a 64 bit build, we’re including a 64 bit Nessus Debian package. 
 #Download the Nessus .deb file and place it in the packages.chroot directory:
 
-mkdir kali-config/common/packages.chroot
-mv Nessus-*amd64.deb kali-config/common/packages.chroot/
+#mkdir kali-config/common/packages.chroot
+#mv Nessus-*amd64.deb kali-config/common/packages.chroot/
 
 
 # We modify the default Kali preseed which disables normal user creation. 
 # We copied this from the debian installer package we initially downloaded.
 
-mkdir -p config/debian-installer
-cp ../debian-installer-*/build/preseed.cfg config/debian-installer/
+#mkdir -p config/debian-installer
+#cp ../debian-installer-*/build/preseed.cfg config/debian-installer/
 #sed -i 's/make-user boolean false/make-user boolean true/' config/debian-installer/preseed.cfg
 #echo "d-i passwd/root-login boolean false" >> config/debian-installer/preseed.cfg
 
